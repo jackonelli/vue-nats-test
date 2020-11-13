c4a2028e10feb2261065d0ae68a339f01b18c4a2
Jakob Lindqvist <jakob.lindqvist.92@gmail.com>
2020-11-13 14:37:26 +0100

    For Jonte's eyes only

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Fuck you, talk soon!"/>
+    <NatsClient msg="Nats client test"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import NatsClient from '@/components/NatsClient.vue'; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    NatsClient,
   },
 })
 export default class Home extends Vue {}
